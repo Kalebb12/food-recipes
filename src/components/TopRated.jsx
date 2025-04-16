5f77294f81fc7f1b9e374a4d3ed910c67d3d0793
Caleb <justcaleb01@gmail.com>
2025-04-16 15:38:45 +0100

    update

--- src/components/TopRated.jsx
+++ src/components/TopRated.jsx
@@ -1,23 +1,33 @@
-import Card from "./Card"
+import { useQuery } from "@tanstack/react-query";
+import Card from "./Card";
 
 const TopRated = () => {
-
   const fetchRecipes = async () => {
-    const res = await fetch('https://dummyjson.com/recipes')
-    const data = await res.json()
-  }
+    const res = await fetch("https://dummyjson.com/recipes?sortBy=rating&order=desc&limit=3");
+    const data = await res.json();
+
+    return data;
+  };
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["recipes"],
+    queryFn: fetchRecipes,
+  });
 
   return (
     <section className="flex flex-col items-center gap-14 my-10 max-w-[1440px] mx-auto">
       <h2 className="font-bold text-3xl">Top Rated Recipes</h2>
 
+      {isPending ? <p>loading....</p> : null}
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 px-10 gap-4">
-        <Card img="/waffles.png" title="waffles"/>
-        <Card title="Pancakes"/>
-        <Card title="Coffee"/>
+        {
+          data?.recipes && data.recipes.map(recipe => {
+            return <Card img={recipe.image} title={recipe.name} />
+          })
+        }
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TopRated
+export default TopRated;
